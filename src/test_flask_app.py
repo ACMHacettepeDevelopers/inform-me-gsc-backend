81a4fcfbe514f6a39e544e59faa54f5722f2dbd2
= <ecesenacs@gmail.com>
2024-02-17 20:47:25 +0300

    added: added the test for podcast creation

--- src/test_flask_app.py
+++ src/test_flask_app.py
@@ -1,5 +1,10 @@
+import os
+
 from flask_app import app
 import json
+
+from pydub import AudioSegment
+
 
 def test_translate_category():
     with app.test_client() as client:
@@ -22,4 +27,30 @@ def test_translate_category():
         # Check if the translation is correct
         assert 'translated_category' in data
         print(data["translated_category"])
+
+def test_create_podcast():
+    with app.test_client() as client:
+        # Make a request to the route
+        response = client.get('/create_podcast', query_string={
+            'country': 'USA',
+            'query': 'example',
+            'count': '1',
+            'podcast_file_name': 'test.mp3'
+        })
+
+        # Check the response status code
+        assert response.status_code == 200
+
+        # Write the bytes to an MP3 file
+        with open('test.mp3', 'wb') as f:
+            f.write(response.data)
+
+        # Load the MP3 file and play it (assuming you have a player installed)
+        audio = AudioSegment.from_file('test.mp3', format='mp3')
+        audio.export('test.wav', format='wav')
+        os.system('start test.wav')  # Open the audio file (Windows)
+
+        ### Clean up the temporary files
+        ##os.remove('test.mp3')
+        ##os.remove('test.wav')
 
