bfc56e8f8a4ca62224f29b82704d651916ff6f08
BerraUrhan <berraurhan2@gmail.com>
2024-02-18 13:56:50 +0300

    .

--- flutter_project/lib/tests/audio.dart
+++ flutter_project/lib/tests/audio.dart
@@ -1,5 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'package:audioplayers/audioplayers.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 Future<void> fetchPodcast() async {
   final response = await http.get(Uri.parse('http://127.0.0.1:5000/create_podcast?country=US&query=economy&count=10&podcast_file_name=podcast.mp3&mode=debug'));
@@ -10,10 +12,6 @@ Future<void> fetchPodcast() async {
     final dir = await getApplicationDocumentsDirectory();
     final file = File('${dir.path}/podcast_local.mp3');
     await file.writeAsBytes(bytes);
-
-    // Play the downloaded audio file
-    AudioPlayer audioPlayer = AudioPlayer();
-    await audioPlayer.play(file.path, isLocal: true);
   } else {
     // If the server returns an error response, throw an exception or handle it accordingly
     throw Exception('Failed to load podcast');
