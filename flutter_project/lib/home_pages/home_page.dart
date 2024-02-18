2138a8c5730611f5a8a543e368ffc4a1d78467e7
BerraUrhan <berraurhan2@gmail.com>
2024-02-18 16:09:23 +0300

    home page organized

--- flutter_project/lib/home_pages/home_page.dart
+++ flutter_project/lib/home_pages/home_page.dart
@@ -41,9 +41,8 @@ class _HomePageState extends State<HomePage> {
   }
 
   final pages = [
-    const Page1(),
-    const Page2(),
-    const Page3(), // profil page
+    const Page1(), // home page
+    const Page2(), // profil page
   ];
 
   @override
@@ -108,7 +107,6 @@ class _HomePageState extends State<HomePage> {
             enableFeedback: false,
             onPressed: () {
               setState(() {
-                //pageIndex = 1;
                 button_clicked == 1 ? button_clicked = 0 : button_clicked = 1;
               });
             },
@@ -148,6 +146,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+// HOME PAGE
 
 class Page1 extends StatelessWidget {
   const Page1({Key? key}) : super(key: key);
@@ -172,29 +171,10 @@ class Page1 extends StatelessWidget {
   }
 }
 
+// PROFIL PAGE
+
 class Page2 extends StatelessWidget {
   const Page2({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 246, 243, 217),
-      child: const Center(
-        child: Text(
-          "Page Number 2",
-          style: TextStyle(
-            color: Color.fromARGB(255, 228, 83, 10),
-            fontSize: 45,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class Page3 extends StatelessWidget {
-  const Page3({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
