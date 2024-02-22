3fefd7282e705bd9d212ea9056a602c96cd53a3a
Ece Sena Etoglu <97747826+EceSenaEtoglu@users.noreply.github.com>
2024-02-23 00:29:14 +0300

    update stt.py
    IT WORKS!!!

--- src/stt.py
+++ src/stt.py
@@ -37,7 +37,7 @@ class STT:
 
         audio = speech.RecognitionAudio(content=content)
         config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            encoding=speech.RecognitionConfig.AudioEncoding.MP3,
             sample_rate_hertz=16000,
             language_code=language_code+"-"+country_code
         )
